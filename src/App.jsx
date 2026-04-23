7672aeb30657555b096683814855d966600a99cd
radzz200 <radhikachoudharyc@gmail.com>
2026-04-23 17:33:33 +0530

    Added deploy for github pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
 import Onboarding from './pages/Onboarding';
 import Dashboard from './pages/Dashboard';
